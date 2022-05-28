07ee047ad001fd7ebe830ecc721d86ccab0ca790
Abstrack <Armon_ah05@outlook.com>
2022-05-28 09:01:53 -0400

    had to restart code, now it works with the same exact code. can view all basic tables.

---
+++ schema.sql
@@ -0,0 +1,32 @@
+DROP DATABASE IF EXISTS the_office;
+CREATE DATABASE the_office;
+USE the_office;
+
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS manager;
+
+
+CREATE TABLE department(
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(25) NOT NULL 
+);
+
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(25) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL, 
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(25) NOT NULL,
+    last_name VARCHAR(25) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id) 
+);
---
+++ server.js
@@ -0,0 +1,92 @@
+const inquirer = require('inquirer');
+const mysql = require('mysql2');
+const config = require('config');
+const user = config.get("server.user");
+const pw = config.get("server.password");
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: user,
+    password: pw,
+    database: 'the_office'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log(`>You are now connected to "The Office" database<`);
+    promptUser();
+});
+
+console.log('+==============================================+');
+console.log('|                    HELLO                     |');
+console.log('|                   WELCOME                    |');
+console.log('+==============================================+');
+
+
+const promptUser = () => {
+    inquirer.prompt([
+        {
+            type:'list',
+            name: 'start',
+            loop: false,
+            message: 'What would you like to do?',
+            choices: ['View all departments',
+                    'View all roles',
+                    'View all employees',
+                    'Add a department',
+                    'Add a role',
+                    'Add a employee',
+                    'Update an Employee role',
+                    'Exit']
+        }
+    ])
+        .then((response) => {
+            switch (response.start) {
+                case "View all departments":
+                    viewDepartments();
+                    break;
+                case "View all roles":
+                    viewRoles();
+                    break;
+                case "View all employees":
+                    viewEmployee();
+                    break;
+                    default:
+                        connection.end();
+            }
+        })
+}
+
+viewDepartments = () => {
+    console.log('Viewing all departments..');
+    const sql = `SELECT department.id, department.name AS department
+                FROM department`;
+
+    connection.query(sql, (err, rows) => {
+        if(err) throw err;
+        console.table(rows);
+        promptUser();
+    });
+};
+
+viewRoles = () => {
+    console.log('Viewing all roles... ');
+    const sql = `SELECT * FROM roles`;
+
+    connection.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        promptUser();
+    });
+};
+
+viewEmployee = () => {
+    console.log('Viewing all Employees... ');
+    const sql = `SELECT * FROM employee`;
+
+    connection.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        promptUser();
+    });
+};
